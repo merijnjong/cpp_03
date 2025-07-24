ca20a4ef50daa347d81d00b0b6b5986167716243
SaltyChickenLeg <merijnjong1@gmail.com>
2025-07-24 15:10:04 +0200

    finished with the exercises

--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -6,21 +6,13 @@
 /*   By: mjong <mjong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 14:11:22 by mjong             #+#    #+#             */
-/*   Updated: 2025/07/24 14:24:28 by mjong            ###   ########.fr       */
+/*   Updated: 2025/07/24 15:09:33 by mjong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCAVTRAP_HPP
 # define SCAVTRAP_HPP
 
-#include <cctype>
-#include <cstring>
-#include <fstream>
-#include <iomanip>
-#include <iostream>
-#include <limits>
-#include <string>
-#include <cmath>
 #include "ClapTrap.hpp"
 
 class ScavTrap : public ClapTrap
--- ex02/ScavTrap.hpp
+++ ex02/ScavTrap.hpp
@@ -6,21 +6,13 @@
 /*   By: mjong <mjong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 14:43:07 by mjong             #+#    #+#             */
-/*   Updated: 2025/07/24 14:50:50 by mjong            ###   ########.fr       */
+/*   Updated: 2025/07/24 15:09:44 by mjong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCAVTRAP_HPP
 # define SCAVTRAP_HPP
 
-#include <cctype>
-#include <cstring>
-#include <fstream>
-#include <iomanip>
-#include <iostream>
-#include <limits>
-#include <string>
-#include <cmath>
 #include "ClapTrap.hpp"
 
 class ScavTrap : public ClapTrap
